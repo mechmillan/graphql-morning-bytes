8466afc9261ab528dc6599a807271e4d01a24037
Alfonso Millan <alfonso.millan@giantmachines.com>
2019-02-11 15:20:04 -0500

    add init with-apollo-client directory CRA

---
+++ with-apollo-client/src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { Wrapper } from "./App.css.js";
+
+class App extends Component {
+  render() {
+    return <Wrapper>init with apollo</Wrapper>;
+  }
+}
+
+export default App;
