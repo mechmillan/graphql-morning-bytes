955cd8f8e950e1ebc963b77fe646b633ba4ad1db
Alfonso Millan <alfonso.millan@giantmachines.com>
2019-02-12 21:12:22 -0500

    add barebones client example

--- client/src/App.css.js
+++ client/src/App.css.js
@@ -1,7 +1,37 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div`
-  color: red;
+export const Data = styled.div`
+  max-width: 500px;
+`;
+
+export const DisplayWrapper = styled.div`
+  align-items: baseline;
+  display: flex;
+  justify-content: space-evenly;
+`;
+
+export const Heading = styled.div`
+  font-weight: bold;
+`;
+
+export const Group = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 10px;
+`;
+
+export const Input = styled.input`
+  font-size: 22px;
   height: 50px;
-  width: 50px;
+  padding: 5px;
+  width: 500px;
+`;
+
+export const Wrapper = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100vh;
 `;
--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,126 @@
 import React, { Component } from "react";
-import { Wrapper } from "./App.css";
+
+import {
+  Data,
+  DisplayWrapper,
+  Heading,
+  Group,
+  Input,
+  Wrapper
+} from "./App.css";
 
 class App extends Component {
   render() {
-    return <Wrapper>hi</Wrapper>;
+    return (
+      <Wrapper>
+        <Company />
+
+        {/* similar for <Quote /> component */}
+      </Wrapper>
+    );
   }
 }
+
+// stateful component
+class Company extends Component {
+  state = {
+    symbol: "",
+    data: {
+      Company: {}
+    }
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    const { symbol } = this.state;
+
+    const BASIC_GRAPHQL_ENDPOINT = "http://localhost:4775/graphql";
+
+    // perform basic fetch/query on Company
+    fetch(BASIC_GRAPHQL_ENDPOINT, {
+      method: "POST",
+      body: JSON.stringify({
+        query: `{
+          Company(symbol: "${symbol}") {
+            CEO
+            companyName
+            description
+            exchange
+          }
+        }`
+      }),
+      headers: { "Content-Type": "application/json" }
+    })
+      .then(res => res.json())
+      .then(res => res.data)
+      .then(data => {
+        this.setState({ data: { Company: data.Company } });
+      })
+      .catch(error => console.error(error));
+  };
+
+  handleInputChange = event => {
+    const { value } = event.currentTarget;
+
+    this.setState({ symbol: value });
+  };
+
+  render() {
+    const { data } = this.state;
+
+    return (
+      <>
+        <form onSubmit={this.handleFormSubmit}>
+          <label>
+            <Input
+              name="symbol"
+              onChange={this.handleInputChange}
+              placeholder="Search by symbol..."
+              type="text"
+            />
+          </label>
+        </form>
+
+        {/* responsible for catching errors */}
+        {data.Company !== null ? (
+          <Display data={data.Company} />
+        ) : (
+          <p>error!</p>
+        )}
+      </>
+    );
+  }
+}
+
+// view-only functional component
+const Display = ({ data }) => {
+  return (
+    <DisplayWrapper>
+      <Group>
+        <Heading>Company Name</Heading>
+
+        <Data>{data.companyName}</Data>
+      </Group>
+
+      <Group>
+        <Heading>CEO</Heading>
+
+        <Data>{data.CEO} </Data>
+      </Group>
+
+      <Group>
+        <Heading>Description</Heading>
+
+        <Data>{data.description} </Data>
+      </Group>
+
+      <Group>
+        <Heading>Exchange</Heading>
+
+        <Data>{data.exchange} </Data>
+      </Group>
+    </DisplayWrapper>
+  );
+};
 
 export default App;
